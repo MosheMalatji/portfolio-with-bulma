94c2f47852c3dbaea9c428550a8213997a132533
Nirmal Kumar Sekar <kagosekar@gmail.com>
2017-12-01 00:25:03 +0530

    1.Added bulma navbar burger toggle 2.Added jquery

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,3 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('Hello Bulma!');
+//bulma navbar burgers toggle logic
+$(function () {
+    var $navbarBurgers = $('.navbar-burger');
+    if ($navbarBurgers.length > 0) {
+        $.each($navbarBurgers, function (i, val) {
+            $(val).on('click', function () {
+                var target = $(val).attr('data-target');
+                var navMenu = $('#'+ target);
+                $(val).toggleClass('is-active');
+                $(navMenu).toggleClass('is-active');
+            });
+        });
+    }
 });
